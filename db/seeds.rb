471f3178495f8c6a1b05f5cba95328e4c9f14867
Marcelo Silva marcelo.cansilva@gmail.com <marcelo.cansilva@gmail.com>
2024-12-07 10:27:29 -0800

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# adding courses
+courses = Course.create([
+  { title: "Ruby on Rails", description: "Learn how to build web applications using Ruby on Rails.", end_date: "2022-12-31" },
+  { title: "React", description: "Learn how to build interactive user interfaces using React.", end_date: "2022-12-31" },
+  { title: "Vue.js", description: "Learn how to build interactive user interfaces using Vue.js.", end_date: "2022-12-31" }
+])
+
+# adding videos
+courses.each do |course|
+  course.videos.create([
+    { title: "Introduction to Ruby on Rails", url: "https://www.youtube.com/watch?v=fmyvWz5TUWg" },
+    { title: "Introduction to React", url: "https://www.youtube.com/watch?v=Ke90Tje7VS0" },
+    { title: "Introduction to Vue.js", url: "https://www.youtube.com/watch?v=4deVCNJq3qc" }
+  ])
+end
